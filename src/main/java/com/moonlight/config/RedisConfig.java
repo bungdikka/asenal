d801773f17c325a7eb5545e4b755310d4b55cb7f
bungdikka <sunlightwk@163.com>
2020-04-26 14:28:34 +0800

    新增 redis lua script

--- src/main/java/com/moonlight/config/RedisConfig.java
+++ src/main/java/com/moonlight/config/RedisConfig.java
@@ -2,9 +2,15 @@ package com.moonlight.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scripting.ScriptSource;
+import org.springframework.scripting.support.ResourceScriptSource;
 
 @Configuration
 public class RedisConfig {
@@ -13,7 +19,24 @@ public class RedisConfig {
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(factory);
+		template.setKeySerializer(new StringRedisSerializer());
 		template.setValueSerializer(new StringRedisSerializer());
 		return template;
+	}
+	
+	@Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+		StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+	
+	@Bean
+	public RedisScript<Boolean> redisScript() {
+		DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+	  ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("META-INF/scripts/checkandset.lua"));
+	  redisScript.setResultType(Boolean.class);
+	  redisScript.setScriptSource(scriptSource);
+	  return redisScript;
 	}
 }
